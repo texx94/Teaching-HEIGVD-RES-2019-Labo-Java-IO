57d103a4151677de569ebf0d9aa5cd5ed4efad38
Loris Gilliand <loris.gilliand@heig-vd.ch>
2019-03-17 12:15:22 +0100

    All FileNumbering test are green

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -4,6 +4,7 @@ import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.logging.Logger;
+import java.lang.*;
 
 /**
  * This class transforms the streams of character sent to the decorated writer.
@@ -11,7 +12,7 @@ import java.util.logging.Logger;
  * It then sends the line number and a tab character, before resuming the write
  * process.
  *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
@@ -19,23 +20,36 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private char prevChar = '\0';
+  private int lineCounter = 1;
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < off + len; ++i)
+      write(str.charAt(i));
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    for(int i = off; i < off + len; ++i)
+      write(cbuf[i]);  }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    if(prevChar == '\0' || (prevChar == '\r' && c != '\n')) {
+      out.write(lineCounter++ + "\t" + (char)c);
+    } else {
+      out.write(c);
+    }
 
+    if(c == '\n') {
+      out.write(lineCounter++ + "\t");
+    }
+
+    prevChar = (char) c;
+  }
 }
